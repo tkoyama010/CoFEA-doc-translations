fedce0f1812600b2f63f9dc31a0e5d6e2eeb024f
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-15 00:20:57 +0900

    📝 Add documentation

--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ For example::
 This conf.py do:
 
 - Specify `locale_dirs` and `gettext_compact`.
-- Overrides source directory as 'CoFEA/doc/`.
+- Overrides source directory as 'CoFEA/docs/`.
 
 """
 import os
@@ -16,16 +16,16 @@ from sphinx.util.pycompat import execfile_
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-execfile_(os.path.join(BASEDIR, "CoFEA/doc/conf.py"), globals())
+execfile_(os.path.join(BASEDIR, "CoFEA/docs/conf.py"), globals())
 
 locale_dirs = [os.path.join(BASEDIR, "locale/")]
 gettext_compact = False
 
-setup_original = setup  # from 'CoFEA/doc/conf.py'
+setup_original = setup  # from 'CoFEA/docs/conf.py'
 
 
 def setup(app):
-    app.srcdir = os.path.join(BASEDIR, "CoFEA/doc/")
+    app.srcdir = os.path.join(BASEDIR, "CoFEA/docs/")
     app.confdir = app.srcdir
 
     setup_original(app)
