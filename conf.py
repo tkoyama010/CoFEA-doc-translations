a851d758ded10131bf9acee6be2bc5593292ffcf
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-15 01:12:27 +0900

    📝 Build RTD (#2)
    * 📝 Add documentation
    
    * 📝 Add documentation
    
    * 🔥 Remove setup function
    
    * 👷 Copy benchmarks directory to root
    
    * 📦️ Add package meshpresso
    
    * 💚 Add .readthedocs.yml
    
    * 💚 Add .readthedocs.yml
    
    * 🔥 Remove .readthedocs.yml
    
    * 🔧 Add setup function
    
    * 🔥 Remove setup_original
    
    * 🔧 Add setup function

---
+++ conf.py
@@ -0,0 +1,31 @@
+# BASEDIR is set by <lang>/conf.py
+"""
+Use "-D language=<LANG>" option to build a localized CoFEA document.
+For example::
+
+    sphinx-build -D language=ja -b html . _build/html
+
+This conf.py do:
+
+- Specify `locale_dirs` and `gettext_compact`.
+- Overrides source directory as 'CoFEA/docs/`.
+
+"""
+import os
+from sphinx.util.pycompat import execfile_
+import shutil
+import sys
+
+shutil.copytree("CoFEA/docs/benchmarks", "benchmarks")
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+execfile_(os.path.join(BASEDIR, "CoFEA/docs/conf.py"), globals())
+
+locale_dirs = [os.path.join(BASEDIR, "locale/")]
+gettext_compact = False
+
+sys.path.insert(0, os.path.abspath("CoFEA/meshpresso"))
+
+def setup(app):
+    app.srcdir = os.path.join(BASEDIR, "CoFEA/docs/")
+    app.confdir = app.srcdir
