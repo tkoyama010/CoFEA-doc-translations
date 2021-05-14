1862305a9a6e9c568dfb3a020e9062165c39a61d
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-15 03:54:30 +0900

    🐛 Fix translation is not reflected

--- conf.py
+++ conf.py
@@ -23,7 +23,6 @@ BASEDIR = os.path.dirname(os.path.abspath(__file__))
 execfile_(os.path.join(BASEDIR, "CoFEA/docs/conf.py"), globals())
 
 locale_dirs = [os.path.join(BASEDIR, "locale/")]
-gettext_compact = False
 
 sys.path.insert(0, os.path.abspath("CoFEA/meshpresso"))
 
