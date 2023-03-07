368d1332e5261fd75798b87d7ccf892a11e550c0
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-07 14:28:39 +0900

    Update update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -6,18 +6,17 @@ set -ex
 # required environment variables
 # SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
 # SPHINXINTL_TRANSIFEX_PROJECT_NAME=cofea-doc
-LOCAL_PYTHON_PATH="/home/runner/work/.venv/bin"
 
 # pull po files from transifex
 cd `dirname $0`
 rm -Rf benchmarks
 cp -r ../CoFEA/docs/benchmarks .
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../CoFEA/docs pot
-$LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
+sphinx-build -T -b gettext ../CoFEA/docs pot
+sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
-$LOCAL_PYTHON_PATH/tx push -s --skip
+tx push -s --skip
 rm -Rf ja
-$LOCAL_PYTHON_PATH/tx pull --silent -f -l ja
+tx pull --silent -f -l ja
 git checkout .tx/config
 rm -Rf benchmarks
